dc01fc018a2a55e41025e986310e489a9314c77a
n1noT <n8no.ttn@gmail.com>
2024-02-05 15:56:53 +0100

    TP 1 05/02/2024

--- src/Controller/LegoController.php
+++ src/Controller/LegoController.php
@@ -2,6 +2,7 @@
 
 
 /* indique où "vit" ce fichier */
+
 namespace App\Controller;
 
 
@@ -15,20 +16,22 @@ use App\Entity\Lego;
 
 
 /* le nom de la classe doit être cohérent avec le nom du fichier */
+
 class LegoController extends AbstractController
 {
-    
+
     private array $legos;
-    
-    public function __construct(){
+
+    public function __construct()
+    {
         $this->legos = [];
 
         $data = file_get_contents('../src/data.json');
         $json = json_decode($data);
-        
-        
-        foreach($json as $lego){
-            $legoModel = new Lego ($lego->id, $lego->name, $lego->collection);
+
+
+        foreach ($json as $lego) {
+            $legoModel = new Lego($lego->id, $lego->name, $lego->collection);
             $legoModel->setDescription($lego->description);
             $legoModel->setPrice($lego->price);
             $legoModel->setPieces($lego->pieces);
@@ -37,56 +40,52 @@ class LegoController extends AbstractController
 
             array_push($this->legos, $legoModel);
         }
-        
+
         return $this->legos;
+    }
+
+    // L’attribute #[Route] indique ici que l'on associe la route
+    // "/" à la méthode home pour que Symfony l'exécute chaque fois
+    // que l'on accède à la racine de notre site.
+
+
+    #[Route('/',)]
+    public function home()
+    {
+
+        // the template path is the relative file path from `templates/`
+        return $this->render('lego.html.twig', ['legos' => $this->legos]);
+    }
+
+    //    #[Route('/creator', )]
+    //    public function creator()
+    //    {
+    //         return $this->render('lego.html.twig', ['legos' => array_filter($this->legos, function($lego) { return $lego->getCollection() === "Creator"; })]);
+    //    }
+
+    //    #[Route('/star_wars', )]
+    //    public function starWars()
+    //    {
+    //         return $this->render('lego.html.twig', ['legos' => array_filter($this->legos, function($lego) { return $lego->getCollection() === "Star Wars"; })]);
+    //    }
+    //    #[Route('/creator_expert', )]
+    //    public function creatorExpert()
+    //    {
+    //         return $this->render('lego.html.twig', ['legos' => array_filter($this->legos, function($lego) { return $lego->getCollection() === "Creator Expert"; })]);
+    //    }
+    
+    #[Route('/{collection}', 'filter_by_collection', requirements: ['collection' => '(creator|star_wars|creator_expert)'])]
+    public function filter($collection): Response
+    {
+        
+        return $this->render('lego.html.twig', ['legos' => array_filter($this->legos, function($lego) use ($collection) {return strtolower($lego->getCollection()) == str_replace('_',' ', strtolower($collection));})]);
         
     }
 
-   // L’attribute #[Route] indique ici que l'on associe la route
-   // "/" à la méthode home pour que Symfony l'exécute chaque fois
-   // que l'on accède à la racine de notre site.
 
-
-   #[Route('/', )]
-   public function home()
-   {
-        
-         // the template path is the relative file path from `templates/`
-         return $this->render('lego.html.twig', ['legos' => $this->legos]);
-   }
-
-//    #[Route('/creator', )]
-//    public function creator()
-//    {
-//         return $this->render('lego.html.twig', ['legos' => array_filter($this->legos, function($lego) { return $lego->getCollection() === "Creator"; })]);
-//    }
-
-//    #[Route('/star_wars', )]
-//    public function starWars()
-//    {
-//         return $this->render('lego.html.twig', ['legos' => array_filter($this->legos, function($lego) { return $lego->getCollection() === "Star Wars"; })]);
-//    }
-//    #[Route('/creator_expert', )]
-//    public function creatorExpert()
-//    {
-//         return $this->render('lego.html.twig', ['legos' => array_filter($this->legos, function($lego) { return $lego->getCollection() === "Creator Expert"; })]);
-//    }
-
-#[Route('/{collection}', 'filter_by_collection')]
-public function filter($collection): Response
-{
-    // return $this->render('lego.html.twig', ['legos' => array_filter($this->legos, function($lego) use ($collection) { return $lego->getCollection() == $collection;})]);
-
-}
-
-
-   #[Route('/me', )]
-   public function me()
-   {
-       die("Nino.");
-   }
-
-    
-
-    
+    #[Route('/me',)]
+    public function me()
+    {
+        die("Nino.");
+    }
 }
